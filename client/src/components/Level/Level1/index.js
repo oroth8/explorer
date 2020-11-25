17aeec9696ee88fe72e96bd169214216309e6602
Justin Albert <jalbert@carthage.edu>
2020-11-24 19:16:08 -0600

    prototype of first level working

---
+++ client/src/components/Level/Level1/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./style.css";
+import imageMap from "./map.json";
+
+const style={
+  backgroundImage: {
+    position: "fixed",
+    top: "0",
+    height: "100vh",
+    width: "100%"
+  },
+  header: {
+    backgroundColor: "white",
+    color: "black",
+    position: "fixed",
+    top: "0",
+    left: "0",
+    zIndex: "1"
+  }
+};
+
+
+// Takes information from employee list and renders it nicely, including a picture (only 2 pictures to choose from, one male one female) give the full name (first middle initial last), email, location and time at company.
+function Level1()  {
+  
+const [location, setLocation]=React.useState({
+  "name":"Earth",
+  "id": 0
+});
+
+let getLandingSpot=(name, id) =>{
+  setLocation({...location, name, id});
+};
+
+
+
+
+        return (
+          <React.Fragment>
+          <img src="./img/levelimgs/earthlvl.jpg" style={style.backgroundImage} alt="Level 1"/>
+          <h2 style={style.header}>You are in {location.name}!</h2>
+          {imageMap.map((item,index)=> (
+            <div style={item.style} onClick={(e)=> {
+              getLandingSpot(item.name, item.id);
+            }}></div>
+          ))}
+          </React.Fragment>
+        );
+  }
+
+
+
+export default Level1;
--- client/src/components/Level/index.js
+++ client/src/components/Level/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import Level1 from "./Level1";
 
 const style={
   backgroundImage: {
@@ -14,7 +15,7 @@ const style={
     left: "40%",
     width: "5%"
   }
-}
+};
 
 // Takes information from employee list and renders it nicely, including a picture (only 2 pictures to choose from, one male one female) give the full name (first middle initial last), email, location and time at company.
 function Level()  {
@@ -24,25 +25,28 @@ function Level()  {
     position: "fixed",
     top: "20%",
     left: "40%",
-    transition: "all .5s",
-    width: "5%"
-  })
+    transition: "all 1s",
+    width: "40px",
+    height: "20px"
+  });
 
   let getLocation= (e) =>{
-    let top=e.pageY+"px";
-    let left=e.pageX+"px";
+    let top=e.pageY-10+"px";
+    let left=e.pageX-20+"px";
     setAvatarState({...avatarState, top, left});
+
+
   }
 
 
       
   
         return (
-          <React.Fragment>
-          <img style={style.backgroundImage} src="./img/levelimgs/earthlvl.jpg" onClick={(e) => {getLocation(e)}}/>
+          <div onClick={(e) => {getLocation(e)}}>
+          <Level1  />
 
           <img style={avatarState} src="./img/avatar.png"/>
-          </React.Fragment>
+          </div>
         );
   }
 
