b4a9474a116bed3f5ec589c62b7980dded5f58d5
Owen Roth <rothowen27@gmail.com>
2020-11-25 12:31:10 -0600

    front-end-register working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // Dependencies
 const express = require("express");
 const routes = require("./routes");
+const path = require("path");
 const connectDB = require("./config/db");
 
 // Express instance
@@ -26,6 +27,19 @@ if (process.env.NODE_ENV === "production") {
 
 // API and View Routes
 app.use(routes);
+// Auth routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 // Start the server
 app.listen(PORT, () => {
