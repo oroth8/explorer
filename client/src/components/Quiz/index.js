d1da77a5d9d4ce589cfacf91db7683b0b197163e
k1te <kmiller343@gmail.com>
2020-11-25 13:08:17 -0600

    rewrote Quiz component to use questions prop

--- client/src/components/Quiz/index.js
+++ client/src/components/Quiz/index.js
@@ -1,38 +1,41 @@
 import React, { useState } from "react";
 
-function Quiz() {
 
-    const testQuestions = [
-        {
-            questionText: "What is the largest city in North America?",
-            answerChoices: [
-                { answerText: "New York", isCorrect: false },
-                { answerText: "Toronto", isCorrect: false },
-                { answerText: "Los Angeles", isCorrect: false},
-                { answerText: "Mexico City", isCorrect: true }
-            ]
-        },
-        {
-            questionText: "The tallest mountain in North America, Denali (pictured below), can be found in what US state?",
-            answerChoices: [
-                { answerText: "Colorado", isCorrect: false },
-                { answerText: "Alaska", isCorrect: true },
-                { answerText: "Washington", isCorrect: false },
-                { answerText: "Montana", isCorrect: false }
-            ],
-            questionImg: "../img/denali.jpg"
-        },
-        {
-            questionText: "What body of water borders North America to the West?",
-            answerChoices: [
-                { answerText: "Atlantic Ocean", isCorrect: false },
-                { answerText: "Gulf of Mexico", isCorrect: false },
-                { answerText: "Pacific Ocean", isCorrect: true },
-                { answerText: "Indian Ocean", isCorrect: false }
-            ],
-            questionImg: "../img/NA.jpg"
-        }
-    ]
+
+function Quiz({questions}) {
+
+    
+    // const testQuestions = [
+    //     {
+    //         questionText: "What is the largest city in North America?",
+    //         answerChoices: [
+    //             { answerText: "New York", isCorrect: false },
+    //             { answerText: "Toronto", isCorrect: false },
+    //             { answerText: "Los Angeles", isCorrect: false},
+    //             { answerText: "Mexico City", isCorrect: true }
+    //         ]
+    //     },
+    //     {
+    //         questionText: "The tallest mountain in North America, Denali (pictured below), can be found in what US state?",
+    //         answerChoices: [
+    //             { answerText: "Colorado", isCorrect: false },
+    //             { answerText: "Alaska", isCorrect: true },
+    //             { answerText: "Washington", isCorrect: false },
+    //             { answerText: "Montana", isCorrect: false }
+    //         ],
+    //         questionImg: "../img/denali.jpg"
+    //     },
+    //     {
+    //         questionText: "What body of water borders North America to the West?",
+    //         answerChoices: [
+    //             { answerText: "Atlantic Ocean", isCorrect: false },
+    //             { answerText: "Gulf of Mexico", isCorrect: false },
+    //             { answerText: "Pacific Ocean", isCorrect: true },
+    //             { answerText: "Indian Ocean", isCorrect: false }
+    //         ],
+    //         questionImg: "../img/NA.jpg"
+    //     }
+    // ]
 
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [showScore, setShowScore] = useState(false);
@@ -44,7 +47,7 @@ function Quiz() {
         }
 
         const nextQuestion = currentQuestion + 1;
-        if (nextQuestion < testQuestions.length) {
+        if (nextQuestion < questions.length) {
             setCurrentQuestion(nextQuestion);
         } else {
             setShowScore(true);
@@ -55,19 +58,19 @@ function Quiz() {
 		<div className='quiz container'>
 			{showScore ? (
 				<div className='score-section'>
-					You scored {score} out of {testQuestions.length}!
+					You scored {score} out of {questions.length}!
 				</div>
 			) : (
 				<>
 					<div className='question-section'>
 						<div className='q-count'>
-							<span>Question {currentQuestion + 1}</span>/{testQuestions.length}
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
 						</div>
-						<div className='question-text'>{testQuestions[currentQuestion].questionText}</div>
-                        <img src={testQuestions[currentQuestion].questionImg} />
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+                        <img src={questions[currentQuestion].questionImg} />
 					</div>
 					<div className='answer-section'>
-						{testQuestions[currentQuestion].answerChoices.map((answerOption) => (
+						{questions[currentQuestion].answerChoices.map((answerOption) => (
 							<button onClick={() => handleUserAnswer(answerOption.isCorrect)}>{answerOption.answerText}</button>
 						))}
 					</div>
