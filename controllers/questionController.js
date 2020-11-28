618016eb87629c42e351db2df460931b66e8b699
k1te <kmiller343@gmail.com>
2020-11-28 17:06:19 -0600

    updated getQuestions method

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -2,8 +2,7 @@ const db = require("../models");
 
 module.exports = {
   getQuestions: function (req, res) {
-    console.log(req);
-    db.Question.find({ level: "North America" })
+    db.Question.find({ level: "North America"})
       .then((questions) => res.json(questions))
       .catch((error) => res.status(422).json(error));
   },
