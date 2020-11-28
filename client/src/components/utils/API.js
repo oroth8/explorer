167315b8904a55c89f9bc0b23284d1bb0df4a25d
k1te <kmiller343@gmail.com>
2020-11-28 17:05:50 -0600

    created getQuizQuestions API call

--- client/src/components/utils/API.js
+++ client/src/components/utils/API.js
@@ -6,3 +6,19 @@ export async function getNewCharacterPortrait(){
 export async function saveCharacter(characterObj){
     return axios.post("/api/character/create",characterObj); 
 }
+
+export function getQuizQuestions(){
+    return axios.get("/api/questions")
+}
+
+// export default {
+//     getNewCharacterPortrait: async function() {
+//         return axios.get("https://randomuser.me/api/?inc=picture");
+//     },
+//     saveCharacter: async function(characterObj) {
+//         return axios.post("/api/character/create",characterObj); 
+//     },
+//     getQuizQuestions: function() {
+//         return axios.get("/api/question")
+//     }
+// }
