e965d3d67f2444b170793b207642eb2fa29a5021
k1te <kmiller343@gmail.com>
2020-11-28 17:06:41 -0600

    added questions route to index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const genericRoutes = require("./generic");
 const characterRoute = require("./character");
+const questionsRoute = require("./questions");
 
 // generic routes
 router.use("/generic", genericRoutes);
-router.use("/character", characterRoute)
+router.use("/character", characterRoute);
+router.use("/questions", questionsRoute);
 module.exports = router;
