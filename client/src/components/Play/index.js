387c54efa5759f7dbd5f1d2f4286a6afb33d4ac7
Justin Albert <jalbert@carthage.edu>
2020-12-01 09:41:21 -0600

    added link to quiz in game menu

--- client/src/components/Play/index.js
+++ client/src/components/Play/index.js
@@ -4,8 +4,9 @@ import "./style.css";
 
 const style={
   img: {
-    width: "90%",
-    height: "auto"
+    width: "600px",
+    maxWidth: "100%",
+    height: "200px"
   }
 }
 
@@ -15,14 +16,19 @@ function Play(props) {
         return (
           <div className="row">
             <div className="col-md-6 col-sm-12">
+              <div className="row">
                 <a href="/charactercreation"><h4>View Character</h4><img style={style.img} src="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/11572307115_1194957269_o.jpg" alt="Character"/></a>
+              </div>
+              <div className="row">
+                <a href="/ship"><h4>Manage Ship</h4><img style={style.img} src="/img/avatar.png" alt="Ship"/></a>
+                </div>
             </div>
             <div className="col-md-6 col-sm-12">
                 <div className="row">
                   <a href="/level"><h4>Explore and Learn </h4><img style={style.img} src="/img/levelimgs/lvl1.jpg" alt="Level Choice"/></a>
                 </div>
                 <div className="row">
-                <a href="/ship"><h4>Manage Ship</h4><img style={style.img} src="/img/avatar.png" alt="Ship"/></a>
+                <a href="/na"><h4>Earn Money</h4><img style={style.img} src="/img/questions/denali.jpg" alt="Quiz"/></a>
                 </div>
             </div>
           </div>
