80f0540ebba5087323d2e17c4a8c6e4924ea79d1
Justin Albert <jalbert@carthage.edu>
2020-12-02 10:43:29 -0600

    saving progress

--- client/src/components/Play/index.js
+++ client/src/components/Play/index.js
@@ -1,33 +1,30 @@
 import License from "../Character/components/License"
 import React from 'react';
-import "./style.css";
-
-const style={
-  img: {
-    width: "600px",
-    maxWidth: "100%",
-    height: "200px"
-  }
-}
 
 
 
-function Play(props) {
+
+
+
+function Play() {
         return (
           <div className="row">
             <div className="col-md-6 col-sm-12">
-              <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/11572307115_1194957269_o.jpg" alt="Character"/>
-                <div class="card-body">
-                    <a href="/viewcharacter"><h5 class="card-text">Character Management</h5></a>
+              <a href="/viewcharacter"> <License /></a>
+            </div>
+
+
+            <div className="col-md-6 col-sm-12">
+              <div className="card" >
+                <img className="card-img-top" src="/img/levelimgs/lvl1.jpg" alt="Character"/>
+                <div className="card-body">
+                    <a href="/level"><h5 className="card-text">Explore and Learn</h5></a>
                 </div>
               </div>
             </div>      
              
      
-            <div className="col-md-6 col-sm-12">
-            <License />
-            </div>
+
           </div>
 
 
