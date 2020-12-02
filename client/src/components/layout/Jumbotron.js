3c1a5e446c6aa8fa912146ef3c2313c88354655f
Owen Roth <rothowen27@gmail.com>
2020-12-02 14:13:40 -0600

    added svg fix and wave aninamtion

--- client/src/components/layout/Jumbotron.js
+++ client/src/components/layout/Jumbotron.js
@@ -1,35 +1,38 @@
-import React, { useContext } from "react";
+import React, { Fragment, useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../../context/auth/authContext";
 import styled from "styled-components";
 import img from "../img/space.jpg";
+import Wave from "./Wave";
 
 const Jumbotron = () => {
   const authContext = useContext(AuthContext);
   const { user } = authContext;
   return (
-    <JumboStyle className="jumbotron jumbotron-fluid">
-      <TS className="container">
-        <span className="display-4">
-          {!user ? "EXPLORER" : `Hello ${user.name},`}
-        </span>
-        <p className="lead">
-          Get ready to explore the world by navigating through NASA related
-          questions and fun.
-        </p>
-        {/* <hr className="my-4"></hr> */}
-        <p id="text">Begin your educational journey today...</p>
-        {!user ? (
-          <Link to="register">
-            <button>Register</button>
-          </Link>
-        ) : (
-          <Link to="/play">
-            <button>Play Now!</button>
-          </Link>
-        )}
-      </TS>
-    </JumboStyle>
+    <Fragment>
+      <JumboStyle className="jumbotron jumbotron-fluid">
+        <TS className="container">
+          <span className="display-4">
+            {!user ? "EXPLORER" : `Hello ${user.name},`}
+          </span>
+          <p className="lead">
+            Get ready to explore the world by navigating through NASA related
+            questions and fun.
+          </p>
+          {/* <hr className="my-4"></hr> */}
+          <p id="text">Begin your educational journey today...</p>
+          {!user ? (
+            <Link to="register">
+              <button>Register</button>
+            </Link>
+          ) : (
+            <Link to="/play">
+              <button>Play Now!</button>
+            </Link>
+          )}
+        </TS>
+      </JumboStyle>
+    </Fragment>
   );
 };
 
