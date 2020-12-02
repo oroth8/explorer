456c8990158f893a660af985862088f6e99726dd
Justin Albert <jalbert@carthage.edu>
2020-12-02 10:28:19 -0600

    creating cards on game menu

--- client/src/components/Play/index.js
+++ client/src/components/Play/index.js
@@ -16,17 +16,20 @@ function Play(props) {
         return (
           <div className="row">
             <div className="col-md-6 col-sm-12">
-              <div className="row">
-                <a href="/viewcharacter"><h4>View Character</h4><img style={style.img} src="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/11572307115_1194957269_o.jpg" alt="Character"/></a>
+              <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/11572307115_1194957269_o.jpg" alt="Character"/>
+                <div class="card-body">
+                    <a href="/viewcharacter"><h5 class="card-text">Character Management</h5></a>
+                </div>
+                </div>
               </div>
-            </div>
             <div className="col-md-6 col-sm-12">
-                <div className="row">
-                  <a href="/level"><h4>Explore and Learn </h4><img style={style.img} src="/img/levelimgs/lvl1.jpg" alt="Level Choice"/></a>
+              <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/11572307115_1194957269_o.jpg" alt="Character"/>
+                <div class="card-body">
+                    <a href="/level"><h5 class="card-text">Explore and Learn</h5></a>
                 </div>
-                <div className="row">
-                <a href="/ship"><h4>Manage Ship</h4><img style={style.img} src="/img/avatar.png" alt="Ship"/></a>
-                </div>
+              </div>
             </div>
           </div>
 
