5ec7baa87cf1668df7d15f81538bbc8021f0600b
k1te <kmiller343@gmail.com>
2020-12-02 10:46:06 -0600

    added character/auth context

--- client/src/components/Quiz/index.js
+++ client/src/components/Quiz/index.js
@@ -1,15 +1,50 @@
-import React, { useEffect, useState } from "react";
-import { getQuizQuestions } from "../utils/API";
-const questions = require("../../NAquestions.json");
+import React, { useEffect, useState, useContext } from "react";
+import { loadCharacter } from "../utils/API";
+import AuthContext from "../../context/auth/authContext";
+import { useCharacterContext } from "../../context/character/CharacterContext";
+
+
+
 
 function Quiz({ questions }) {
+  const [state, dispatch] = useCharacterContext();
+  const authContext = useContext(AuthContext);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showScore, setShowScore] = useState(false);
   const [score, setScore] = useState(0);
+  
+
+  let userId;
+
+  useEffect(() => {
+    authContext.loadUser();
+
+    if (authContext.user) {
+      userId = authContext.user._id;
+      getCharacter(userId);
+    }
+  }, [authContext.loading]);
+
+  console.log(authContext);
+  
+
+
+
+  const getCharacter = (userId) => {
+    dispatch({ type: "LOADING" });
+    loadCharacter(userId).then((res) => {
+      if (res.data) dispatch({ type: "UPDATE_CHARACTER", char: res.data });
+      else dispatch({ type: "ERROR_NO_CHARACTER" });
+    });
+  };
+  
+  
 
   const handleUserAnswer = (isCorrect) => {
     if (isCorrect) {
       setScore(score + 1);
+      console.log(authContext);
+      console.log(state);
     }
 
     const nextQuestion = currentQuestion + 1;
