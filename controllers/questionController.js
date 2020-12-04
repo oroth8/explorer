7bfbe6665c0b49a8647113b57eb862d0f3e3befc
k1te <kmiller343@gmail.com>
2020-12-04 11:25:01 -0600

    wrote getCategories function for db query

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -6,4 +6,11 @@ module.exports = {
       .then((questions) => res.json(questions))
       .catch((error) => res.status(422).json(error));
   },
+  getCategories: function (req, res) {
+    db.Question.aggregate([
+      {$group: {_id: "$level", map: {$first: "$map"}}},
+      {$sort: {_id: 1}}
+    ]).then((categories) => res.json(categories))
+      .catch((error) => res.status(422).json(error));
+  }
 };
