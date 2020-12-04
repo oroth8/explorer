5b9a0598e20ef5068ed6d950bef2562bc94fe0e1
k1te <kmiller343@gmail.com>
2020-12-04 11:25:20 -0600

    added route for getting categories

--- routes/api/questions.js
+++ routes/api/questions.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const questionController = require("../../controllers/questionController");
 
 router.route("/").get(questionController.getQuestions);
+router.route("/category").get(questionController.getCategories);
 
 module.exports = router;
