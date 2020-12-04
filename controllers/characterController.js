8daa0e501c1c24c24069c819959504681ff35c02
Benjamin Dionysus <benjamin@sixbynine.com>
2020-12-03 18:18:38 -0600

    Cleaned up code, added catch() and comments

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,32 +1,30 @@
 const db = require('../models/Character');
 
-// Export Controller Methods
-module.exports = {
+module.exports = {    
+    // Many thanks to Paul and to:
+    // https://stackoverflow.com/questions/17244363/mongoose-findoneandupdate-upsert-id-null  
+
+    // When the user creates a new character, the API calls this function. findOneAndUpdate,
+    // with the upsert keyword. If we have passed in a chracter _id, the it will find that 
+    // document in the database and update it. If we have not passed in an id, it will 
+    // create a new document instead.
     create: function(req, res) {
-        // console.log("Creating"+data);
-        // Many thanks to Paul and to:
-        // https://stackoverflow.com/questions/17244363/mongoose-findoneandupdate-upsert-id-null  
         db.findOneAndUpdate({userId:req.body.userId}, req.body, {new:true, upsert:true})
-        .then((data)=>{      
-                  
-            res.json(data)
-        }) 
-        .catch(err=>{
-            console.log("Controller error!!!");
-            console.log(err);
-            
-            
-        })
-    },
+            .then((data)=>{                        
+                res.json(data)
+            }) 
+            .catch(err=>{
+                console.error("Erorr in CharacterController, findOneAndUpdate:"+err);
+                res.status(500).json({ msg: err });                    
+            })
+        },
+        // Return the document with a userId matching the passed id.
     load: function(req, res){        
-        db.findOne(
-            {
-                userId:req.params.id
-            }, (err, data)=>{
-                if(err) res.json({err})
-                else res.json({data})
-            }
-        )
+        db.findOne({userId:req.params.id}, (err, data)=>{
+            if(err) 
+                res.status(500).json({ msg: err });             
+            else res.json({data})
+        })
     }
 };
 
