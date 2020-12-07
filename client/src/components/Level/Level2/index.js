99a6c12022dd64968d886ed8188a1ad057293b40
Justin Albert <jalbert@carthage.edu>
2020-12-07 13:38:52 -0600

    adding import gamenav

--- client/src/components/Level/Level2/index.js
+++ client/src/components/Level/Level2/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./style.css";
 import imageMap from "./map.json";
-import Location from "../Location"
+import Location from "../Location";
+import GameNav from "../../layout/GameNav";
 
 const style={
   container: {
