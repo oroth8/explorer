581884a965fdda9c692600a559cb7b6a62a5b657
Benjamin Dionysus <benjamin@sixbynine.com>
2020-12-09 13:01:54 -0600

    added

---
+++ client/src/components/Chat/socketMaker.js
@@ -0,0 +1,18 @@
+const io = require("socket.io-client");
+const socket=io(`http://${window.location.hostname}:3001`, {transports: ['websocket']});
+
+socket.on('USER_MESSAGE',(msg) => {
+    if(msg){
+      console.log("Received: "+msg);      
+      socket.payload=msg;
+    //   let temp=messagesString;
+    //   setMessages(temp+"\n"+msg);
+   }
+  });   
+  socket.on('LOGIN_MESSAGE',(msg) => {
+    if(msg){
+      console.log("Received: "+msg);      
+      socket.login=msg;
+   }
+  });   
+export default socket;
