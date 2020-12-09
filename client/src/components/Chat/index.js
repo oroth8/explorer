b25b4f2183875aef9354061c0acefa1f9d08dd4c
Benjamin Dionysus <benjamin@sixbynine.com>
2020-12-09 11:07:49 -0600

    moved listeners

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -1,16 +1,48 @@
 import React, { useEffect, useContext, useState } from "react";
 import "./style.css";
 import AuthContext from "../../context/auth/authContext";
+
+const io = require("socket.io-client");
+let socket = io({transports: ['websocket']});
+
+
+// if (process.env.NODE_ENV === 'production') {
+//   socket=io.connect();
+// } else {
+//   socket=io.connect('http://localhost:3001');
+// }
+
 const style={
   back: {
     background: "rgba(0,0,0,0)"
   }
 }
+socket.once('USER_MESSAGE',(msg) => {
+  if(msg){
+    console.log("Received: "+msg);      
+    // let temp=messagesString;
+    console.log(msg);
+     
+     
+ }
+});   
+socket.on('LOGIN_MESSAGE',(msg) => { 
+  if(msg){
+    // let temp=messagesString;
+    // setMessages(temp+"\n"+msg);
+    // temp(msg);
+    console.log(msg);
+    
+ }
+});
+
+
+
+
 export default function Chat(){
-  const authContext = useContext(AuthContext);
+  
   const [messagesString, setMessages] = useState("");
-  const io = require("socket.io-client");
-  const socket = io({transports: ['websocket']});
+  const authContext = useContext(AuthContext);
   useEffect(() => {
     if (authContext.user) { 
       socket.once('connect', () => {
@@ -22,29 +54,22 @@ export default function Chat(){
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [authContext.loading]);
 
+
+  function temp(msg){
+    let t=messagesString;
+    t=t+"\n"+msg;
+    setMessages(t);
+  }
+
   function submitMessage(e) {
     e.preventDefault();
     if (e.target.message.value) {
       console.log("Trying to send "+e.target.message.value);      
-      socket.emit('TELL_EVERYONE', authContext.user.name+": "+e.target.message.value);
+      // socket.emit('TELL_EVERYONE', authContext.user.name+": "+e.target.message.value);
+      socket.emit('TELL_EVERYONE', e.target.message.value);
       e.target.message.value="";
     }
   }
- 
-
-  socket.once('USER_MESSAGE',(msg) => {
-    if(msg){
-      console.log("Received: "+msg);      
-      let temp=messagesString;
-      setMessages(temp+"\n"+msg);
-   }
-  });   
-  socket.on('LOGIN_MESSAGE',(msg) => { 
-    if(msg){
-      let temp=messagesString;
-      setMessages(temp+"\n"+msg);
-   }
-  });
 
     return (
       <div className="container" id="menucard" style={style.back}>
